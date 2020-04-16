890bc143538b5a499121715d6b8b4e0797486349
Knut Melvær <knut.melvaer@gmail.com>
2020-04-16 22:34:58 +0200

    render markdown in slack content

--- schemas/components/threadPreview/index.js
+++ schemas/components/threadPreview/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import unescape from 'lodash/unescape';
+import ReactMarkdown  from'react-markdown'
 
 const ThreadPreview = ({ document }) => {
   const { displayed = {} } = document
@@ -6,7 +8,7 @@ const ThreadPreview = ({ document }) => {
   return (
     <div>
       <ul>
-        {thread.map(({ _key, author, content, timestamp  }) => <li key={_key}>👤{author}: {content} ({new Date(timestamp * 1000).toDateString()})</li>)}
+        {thread.map(({ _key, author, content, timestamp  }) => <li key={_key}>👤{author}: <ReactMarkdown source={unescape(content)} /> ({new Date(timestamp * 1000).toDateString()})</li>)}
       </ul>
       {/* <pre>{JSON.stringify(displayed, null, 2)}</pre> */}
     </div>

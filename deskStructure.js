7bed7c4ce74f7b911fc3e810d03e7377f3b6694e
Knut Melvær <knut.melvaer@gmail.com>
2020-04-25 12:27:13 +0200

    add my tickets, tickets by status

--- deskStructure.js
+++ deskStructure.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import S from '@sanity/desk-tool/structure-builder'
 import documentStore from 'part:@sanity/base/datastore/document'
+import userStore from 'part:@sanity/base/user'
+
 import { map } from 'rxjs/operators'
 import Icon from './schemas/components/icon'
 import ThreadPreview from './schemas/components/threadPreview'
@@ -8,25 +10,77 @@ import ThreadPreview from './schemas/components/threadPreview'
 const hiddenDocTypes = listItem =>
   !['person', 'ticket', 'tagOption'].includes(listItem.getId())
 
+const ticketDocumentNode = docId =>
+  S.document()
+    .documentId(docId)
+    .views([S.view.form(), S.view.component(ThreadPreview).title('Thread')])
+
 export default () =>
   S.list()
     .title('Content')
     .items([
       S.listItem()
-        .title('Tickets')
+        .title('My open tickets')
+        .schemaType('ticket')
+        .icon(() => <Icon emoji="⏳" />)
+        .child(
+          userStore.currentUser.pipe(
+            map(({ user }) => {
+              const { role } = user
+              console.log(user)
+              return S.documentList('ticket')
+                .title('My open tickets')
+                .filter('_type == $type && status == "open" && assigned->sanityId == $userId')
+                .params({ type: 'ticket', userId: user.id })
+                .child(ticketDocumentNode)
+            })
+          )
+        ),
+      S.listItem()
+        .title('All tickets')
         .schemaType('ticket')
         .child(
           S.documentList('ticket')
-            .title('Tickets')
+            .title('All tickets')
             .filter('_type == $type')
             .params({ type: 'ticket' })
-            .child(docId =>
-              S.document()
-                .documentId(docId)
-                .views([
-                  S.view.form(),
-                  S.view.component(ThreadPreview).title('Thread')
-                ])
+            .child(ticketDocumentNode)
+        ),
+      S.listItem()
+        .title('Open tickets')
+        .schemaType('ticket')
+        .child(
+          S.documentList('ticket')
+            .title('Open tickets')
+            .filter('_type == $type && status == "open"')
+            .params({ type: 'ticket' })
+            .child(ticketDocumentNode)
+        ),
+      S.listItem()
+        .title('Resolved tickets')
+        .schemaType('ticket')
+        .icon(() => <Icon emoji="✅" />)
+        .child(
+          S.documentList('ticket')
+            .title('Resolved tickets')
+            .filter('_type == $type && status == "resolved"')
+            .params({ type: 'ticket' })
+            .child(ticketDocumentNode)
+        ),
+      S.listItem()
+        .title('Tickets by Agents')
+        .schemaType('person')
+        .child(
+          S.documentList('person')
+            .title('Tickets by Agents')
+            .filter('_type == $type')
+            .params({ type: 'person' })
+            .child(agentID =>
+              S.documentList('ticket')
+                .title('Tickets')
+                .filter('_type == $type && references($agentID)')
+                .params({ type: 'ticket', agentID })
+                .child(ticketDocumentNode)
             )
         ),
       S.listItem()
@@ -64,8 +118,12 @@ export default () =>
                           .pipe(
                             map(documents =>
                               S.documentTypeList('ticket')
-                                .title(`Tickets for “${tag}” (${documents.length})`)
-                                .menuItems(S.documentTypeList('ticket').getMenuItems())
+                                .title(
+                                  `Tickets for “${tag}” (${documents.length})`
+                                )
+                                .menuItems(
+                                  S.documentTypeList('ticket').getMenuItems()
+                                )
                                 .filter(`_id in $ids`)
                                 .params({
                                   ids: documents.map(({ _id }) => _id)
@@ -95,7 +153,9 @@ export default () =>
                     .menuItems(S.documentTypeList('tagOption').getMenuItems())
                     .filter('_type == $type')
                     .params({ type: 'tagOption' })
-                    .canHandleIntent(S.documentTypeList('tagOption').getCanHandleIntent())
+                    .canHandleIntent(
+                      S.documentTypeList('tagOption').getCanHandleIntent()
+                    )
                 ),
               S.listItem()
                 .title('Persons')
@@ -105,7 +165,7 @@ export default () =>
                     .title('Persons')
                     .filter('_type == $type')
                     .params({ type: 'person' })
-                ),
+                )
             ])
         ),
       ...S.documentTypeListItems().filter(hiddenDocTypes)

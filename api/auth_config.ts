e66fb434bd6130d4c5a0040597742ac7df8490ca
Knut Melvær <knut.melvaer@gmail.com>
2020-04-27 11:17:40 +0200

    remove unused file

--- api/auth_config.ts
+++
@@ -1,8 +0,0 @@
-import {NowRequest, NowResponse} from '@now/node';
-
-export default async (req: NowRequest, res: NowResponse) => {
-  res.send({
-    domain: 'sanity-io-demo.eu.auth0.com',
-    clientId: 'cssYOH8VE9jX6i1SIn95svqf4B5Mx32A',
-  });
-};

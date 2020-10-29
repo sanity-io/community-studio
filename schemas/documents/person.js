c18428070254e9b727e896228cdeccc0e4d7d910
Knut Melvær <knut.melvaer@gmail.com>
2020-10-29 15:26:45 +0100

    first iteration of contributor schemas

--- schemas/documents/person.js
+++ schemas/documents/person.js
@@ -7,7 +7,6 @@ export default {
   title: 'Person',
   type: 'document',
   icon: () => <Icon emoji="👤" />,
-
   fields: [
     {
       name: 'name',
@@ -15,30 +14,34 @@ export default {
       title: 'Name'
     },
     {
+      name: 'public',
+      type: 'boolean',
+      title: 'Make my profile public',
+      description:
+        'Do you want to have your profile on sanity.io/community/people/@nickname?'
+    },
+    {
       name: 'nickname',
       title: 'Nickname',
-      type: 'string'
+      type: 'slug',
+      // @todo: Add additional validation to avoid NSFW stuff?
+      // @todo: Add an “are you sure” publish action if this changes
+      description: 'If you change this your current URL will break',
+      options: {
+        source: 'name'
+      }
     },
     {
-      name: 'slackId',
-      title: 'Slack ID',
-      type: 'string'
-    },
-    {
-      name: 'sanityId',
-      title: 'Sanity ID',
-      type: 'string',
-      description: 'Sanity UserID'
-    },
-    {
-      name: 'github',
-      title: 'GitHub Username',
-      type: 'string'
-    },
-    {
-      name: 'image',
+      name: 'photo',
       type: 'image',
-      title: 'Image'
+      title: 'Image',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          description: `Describe the photo for people who can't view it`
+        }
+      ]
     },
     {
       name: 'imageUrl',
@@ -49,17 +52,88 @@ export default {
     {
       name: 'email',
       type: 'email',
-      title: 'email'
+      title: 'Contact email',
+      description:
+        'Initial value from Auth0, but editable if you want to change it'
     },
     {
       name: 'bio',
       type: 'array',
       title: 'Bio',
+      description:
+        'Who are you? What do you do? What have you done? One unexpected thing about yourself.',
       of: [
         {
           type: 'block'
         }
       ]
+    },
+    {
+      name: 'work',
+      type: 'object',
+      title: 'Work',
+      fields: [
+        {
+          name: 'company',
+          type: 'string',
+          title: 'Company name',
+          description:
+            'Name of your workplace, your own company, or “Freelance” etc'
+        },
+        {
+          name: 'url',
+          type: 'url',
+          title: 'Company URL'
+        },
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Job title'
+        }
+      ]
+    },
+    {
+      name: 'social',
+      type: 'object',
+      title: 'Social links',
+      fields: [
+        'Dribbble',
+        'GitLab',
+        'Medium',
+        'Behance',
+        'LinkedIn',
+        'StackOverflow',
+        'Youtube',
+        'Facebook',
+        'Twitch',
+        'Mastodon',
+        'Instagram'
+      ].map(vendor => ({
+        name: vendor.toLowerCase(),
+        title: vendor,
+        type: 'string'
+      }))
+    },
+    {
+      name: 'slackId',
+      title: 'Slack ID',
+      type: 'string'
+      // @todo: Instructions on how to find your slack id
+      // description: ''
+    },
+    {
+      name: 'sanityId',
+      title: 'Sanity ID',
+      type: 'string',
+      description: 'Added automatically on document creation',
+      hidden: true
+    },
+    {
+      name: 'github',
+      title: 'GitHub Username',
+      type: 'string',
+      description: 'Added by Auth0',
+      hidden: true
     }
   ]
 }
---
+++ schemas/objects/studioImage.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'studioImage',
+  type: 'image',
+  title: 'Studio image',
+  options: {
+    hotspot: true
+  },
+  fields: [
+    {
+      name: 'version',
+      type: 'string',
+      title: 'Studio version',
+      options: {
+        isHighlighted: true
+      }
+    },
+    {
+      name: 'caption',
+      type: 'string',
+      title: 'Caption',
+      options: {
+        isHighlighted: true
+      }
+    },
+    {
+      name: 'alt',
+      type: 'string',
+      title: 'Alternative text',
+      description: 'Important for SEO and accessiblity.',
+      options: {
+        isHighlighted: true
+      }
+    }
+  ],
+  preview: {
+    select: {
+      imageUrl: 'asset.url',
+      title: 'caption'
+    }
+  }
+}

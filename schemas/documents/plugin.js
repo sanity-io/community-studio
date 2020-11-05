209bf525c2e631069e44649b0a14af36dd3f1a82
Henrique Doro <opensource@hdoro.dev>
2020-11-05 19:17:44 -0300

    Initial iteration of community plugins

--- schemas/documents/plugin.js
+++ schemas/documents/plugin.js
@@ -7,11 +7,32 @@ export default {
   type: 'document',
   title: 'Plugin or tool',
   icon: () => <Icon emoji="🔌" />,
+  fieldsets: [
+    {
+      name: 'code',
+      title: 'Source code, npm and readme information',
+      options: {collapsible: true, collapsed: false},
+    },
+    // {
+    //   name: 'visual',
+    //   title: 'Looks of your plugin in the community',
+    //   options: {collapsible: true, collapsed: false},
+    // },
+  ],
   fields: [
     {
       name: 'title',
       type: 'string',
       title: 'Plugin/tool name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'headline',
+      title: 'Headline / short description for the tool',
+      description:
+        'Use this space to explain briefly what it is and how it may help other Sanity community members.',
+      type: 'text',
+      rows: 1,
     },
     {
       name: 'slug',
@@ -22,24 +43,48 @@ export default {
       options: {
         basePath: 'sanity.io/plugins',
       },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'authors',
+      type: 'array',
+      title: 'Author(s)',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'person'}],
+        },
+      ],
     },
     {
       name: 'gitUrl',
       type: 'url',
       title: 'Github or Gitlab URL of the repository',
       description: 'The repository for where this code is stored.',
+      fieldset: 'code',
     },
     {
       name: 'readmeUrl',
       type: 'url',
       title: 'URL to the raw readme file',
       description: "We'll use that to render your tool's readme in the Sanity site",
+      validation: (Rule) => Rule.required(),
+      fieldset: 'code',
     },
     {
       name: 'npmUrl',
       type: 'url',
       title: 'npm package URL',
       description: 'In case you deployed it to npm',
+      fieldset: 'code',
+    },
+    // @TODO: does it make sense to provide install commands for npm packages? Such as `npm i metalsmith-sanity`, which isn't applicable to the Sanity studio.
+    {
+      name: 'installWith',
+      type: 'string',
+      title: 'Command to install it in Sanity studio (for studio plugins)',
+      description: 'Ex: sanity install media. Feel free to ignore this if not applicable.',
+      fieldset: 'code',
     },
     // @TODO: turn these into documents
     {
@@ -62,23 +107,12 @@ export default {
         ],
       },
     },
+    // @TODO: turn these into documents
     {
-      name: 'authors',
+      name: 'solutions',
+      title: 'Solutions',
       type: 'array',
-      title: 'Author(s)',
-      of: [
-        {
-          type: 'reference',
-          to: [{type: 'person'}],
-        },
-      ],
-    },
-    {
-      name: 'headline',
-      title: 'Headline / short description for the tool',
-      description:
-        'Use this space to explain briefly what it is and how it may help other Sanity community members.',
-      type: 'text',
+      of: [{type: 'string'}],
     },
     {
       name: 'logo',
@@ -92,17 +126,16 @@ export default {
       name: 'readme',
       title: 'Readme',
       description: 'Populated from the git URL above',
-      type: 'text',
+      type: 'markdown',
+      // hidden: true,
     },
     /**
-     * Missing:
-     * npm (only got markdown so far)
-     * solutions
-     * screenshots
-     * lengthier description
-     * installWith
-     * brandColor
-     * color
+     * Missing / debating:
+     * npm - I think only the markdown portion is valuable
+     * screenshots - wasn't being used, so I removed it
+     * lengthier description - wasn't being used, so I removed it
+     * brandColor - is it truly necessary? What if we generate them automatically?
+     * color - I think this was replaced by brandColor
      */
   ],
 };

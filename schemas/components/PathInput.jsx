e5fc23ba751d10561ca2a493cbc960c549dda7b8
Henrique Doro <opensource@hdoro.dev>
2020-11-05 18:18:08 -0300

    Initial iteration of the new plugin schema

--- schemas/components/PathInput.jsx
+++ schemas/components/PathInput.jsx
@@ -1,71 +1,70 @@
-import React from 'react'
-import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event'
-import DefaultTextInput from 'part:@sanity/components/textinputs/default'
-import DefaultFormField from 'part:@sanity/components/formfields/default'
+import React from 'react';
+import PatchEvent, {set, unset} from 'part:@sanity/form-builder/patch-event';
+import DefaultTextInput from 'part:@sanity/components/textinputs/default';
+import DefaultFormField from 'part:@sanity/components/formfields/default';
 
-import styles from './PathInput.module.css'
+import styles from './PathInput.module.css';
 
-const createPatchFrom = (value) =>
-  PatchEvent.from(value === '' ? unset() : set(value))
-
+const createPatchFrom = (value) => PatchEvent.from(value === '' ? unset() : set(value));
 
 export default class PathInput extends React.Component {
-  inputRef
+  inputRef;
   constructor(props) {
-    super(props)
-    this.inputRef = React.createRef()
-    this.state = { basePath: props.type.options?.basePath || '' }
+    super(props);
+    this.inputRef = React.createRef();
+    this.state = {basePath: props.type.options?.basePath || ''};
   }
 
   focus = () => {
     if (this.inputRef && this.inputRef.current) {
-      this.inputRef.current.focus()
+      this.inputRef.current.focus();
     }
-  }
+  };
+
+  updateValue = (strValue) => {
+    const isSlug = this.props.type.name === 'slug';
+    const patchValue = isSlug ? {_type: 'slug', current: strValue} : strValue;
+    this.props.onChange(createPatchFrom(patchValue));
+  };
 
   /**
    * Avoids trailing slashes, double slashes, spaces, special characters and uppercase letters
    */
   formatSlug = () => {
-    let finalSlug = this.props.value || ''
-    
-    const formatSlugOnBlur = this.props.type.options?.formatSlug !== false
+    const curSlug =
+      typeof this.props.value === 'string' ? this.props.value : this.props.value?.current;
+    let finalSlug = curSlug || '';
+
+    const formatSlugOnBlur = this.props.type.options?.formatSlug !== false;
     if (formatSlugOnBlur) {
       // Removing special characters and other slug needs
       // we split slashes to avoid them being replaced with dashes
       // @TODO: get a slugify function and plug it in the line below
       // finalSlug = slugify(finalSlug)
 
-      finalSlug = finalSlug.toLowerCase().replace(/\s/gmi, '') // placeholder for slugify
+      finalSlug = finalSlug.toLowerCase().replace(/\s/gim, ''); // placeholder for slugify
     }
 
     // Finally, save this final slug to the document
-    this.props.onChange(createPatchFrom(finalSlug))
-  }
+    this.updateValue(finalSlug);
+  };
 
   render() {
-    const { value, onChange, type } = this.props
+    const {value, onChange, type} = this.props;
+    // This field is usable both for strings as well as for slugs, we need to account for these different data structures
+    const strValue = type.name === 'slug' ? value?.current : value;
     return (
-      <DefaultFormField
-        label={type.title || type.name}
-        description={type.description}
-      >
+      <DefaultFormField label={type.title || type.name} description={type.description}>
         <div className={styles.wrapper}>
-          {this.state.basePath && (
-            <div className={styles.url}>
-              {this.state.basePath + '/'}
-            </div>
-          )}
+          {this.state.basePath && <div className={styles.url}>{this.state.basePath + '/'}</div>}
           <DefaultTextInput
-            value={value || ''}
+            value={strValue || ''}
             type="text"
-            onChange={(event) =>
-              onChange(createPatchFrom(event.target.value))
-            }
+            onChange={(event) => this.updateValue(event.target.value)}
             onBlur={this.formatSlug}
           />
         </div>
       </DefaultFormField>
-    )
+    );
   }
 }
--- schemas/documents/plugin.js
+++ schemas/documents/plugin.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Icon from '../components/Icon'
+import React from 'react';
+import Icon from '../components/Icon';
+import PathInput from '../components/PathInput';
 
 export default {
   name: 'plugin',
@@ -13,19 +14,95 @@ export default {
       title: 'Plugin/tool name',
     },
     {
-      name: 'url',
+      name: 'slug',
+      title: 'Where should it appear on the sanity community?',
+      description: '💡 avoid special characters, spaces and uppercase letters.',
+      type: 'slug',
+      inputComponent: PathInput,
+      options: {
+        basePath: 'sanity.io/plugins',
+      },
+    },
+    {
+      name: 'gitUrl',
       type: 'url',
-      title: 'URL'
+      title: 'Github or Gitlab URL of the repository',
+      description: 'The repository for where this code is stored.',
+    },
+    {
+      name: 'readmeUrl',
+      type: 'url',
+      title: 'URL to the raw readme file',
+      description: "We'll use that to render your tool's readme in the Sanity site",
+    },
+    {
+      name: 'npmUrl',
+      type: 'url',
+      title: 'npm package URL',
+      description: 'In case you deployed it to npm',
+    },
+    // @TODO: turn these into documents
+    {
+      title: 'Categories',
+      name: 'categories',
+      type: 'array',
+      of: [{type: 'string'}],
+      options: {
+        layout: 'tags',
+        list: [
+          {value: 'inputComponent', title: 'Input component'},
+          {value: 'studioTool', title: 'Studio tool'},
+          {value: 'assetSource', title: 'Asset source'},
+          {value: 'dashboardWidget', title: 'Dashboard widget'},
+          {value: 'importAndMigration', title: 'Import and migration'},
+          {value: 'clients', title: 'Clients and SDKs'},
+          {value: 'portableText', title: 'Portable Text'},
+          {value: 'groq', title: 'GROQ'},
+          {value: 'other', title: 'Other'},
+        ],
+      },
     },
     {
       name: 'authors',
       type: 'array',
-      title: 'Authors',
+      title: 'Author(s)',
       of: [
         {
-          type: 'reference', to: [{type: 'person'}]
-        }
-      ]
-    }
-  ]
-}
+          type: 'reference',
+          to: [{type: 'person'}],
+        },
+      ],
+    },
+    {
+      name: 'headline',
+      title: 'Headline / short description for the tool',
+      description:
+        'Use this space to explain briefly what it is and how it may help other Sanity community members.',
+      type: 'text',
+    },
+    {
+      name: 'logo',
+      type: 'image',
+      title: 'Logo',
+      description:
+        'Is there any image that describes your project? If you can, provide a transparent PNG to fit nicely in the community.',
+    },
+    // Hidden fields populated automatically
+    {
+      name: 'readme',
+      title: 'Readme',
+      description: 'Populated from the git URL above',
+      type: 'text',
+    },
+    /**
+     * Missing:
+     * npm (only got markdown so far)
+     * solutions
+     * screenshots
+     * lengthier description
+     * installWith
+     * brandColor
+     * color
+     */
+  ],
+};

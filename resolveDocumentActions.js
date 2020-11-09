14ae35ce3381947d4151f37729086a4111213c3f
Henrique Doro <opensource@hdoro.dev>
2020-11-09 18:13:52 -0300

    Preventing the deletion/duplication of `person`

--- resolveDocumentActions.js
+++ resolveDocumentActions.js
@@ -2,10 +2,18 @@ import defaultResolve from 'part:@sanity/base/document-actions';
 import publishPluginAction from './publishPluginAction';
 
 export default function resolveDocumentActions(props) {
+  console.log(props, window._sanityUser);
   if (props.type === 'plugin') {
     return [
       publishPluginAction,
       ...defaultResolve(props).filter((action) => action.name !== 'PublishAction'),
+    ];
+  }
+  if (props.type === 'person' && window._sanityUser?.provider === 'external') {
+    return [
+      ...defaultResolve(props).filter(
+        (action) => action.name !== 'DeleteAction' && action.name !== 'DuplicateAction'
+      ),
     ];
   }
   return defaultResolve(props);

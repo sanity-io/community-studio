7daa610c35c06ffc7e2d1d5a7796c85175fef1c7
Henrique Doro <opensource@hdoro.dev>
2020-11-13 16:20:19 -0300

    curatedContribution v1

---
+++ api/curate-contribution.ts
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+import sanityClient from '@sanity/client';
+import {NowRequest, NowResponse} from '@now/node';
+
+const projectId = process.env.SANITY_PROJECT_ID;
+
+const client = sanityClient({
+  projectId,
+  dataset: process.env.SANITY_DATASET,
+  token: process.env.SANITY_WRITE_TOKEN,
+  useCdn: false,
+});
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const {docId, contributionType} = req.query;
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  if (typeof docId !== 'string') {
+    return res.status(400).json({
+      error: 'Missing doc id',
+    });
+  }
+  if (typeof contributionType !== 'string') {
+    return res.status(400).json({
+      error: 'Missing contribution type',
+    });
+  }
+
+  const curatedDoc = {
+    _id: `curated.${docId}`,
+    _type: 'curatedContribution',
+    contribution: {
+      _type: 'reference',
+      _ref: docId
+    },
+  }
+
+  try {
+    const res = await client.createIfNotExists(curatedDoc)
+
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "We couldn't create the document",
+    });
+  }
+};

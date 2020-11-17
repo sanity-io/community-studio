743aab35bf67dc2d37a83899fbc09bd75c6f1596
Henrique Doro <opensource@hdoro.dev>
2020-11-17 11:29:35 -0300

    Renaming tool publish action for clarity

--- parts/resolveDocumentActions.js
+++ parts/resolveDocumentActions.js
@@ -1,22 +1,25 @@
 import defaultResolve from 'part:@sanity/base/document-actions';
 import PublishContributionAction from './publishContributionAction';
-import PublishPluginAction from './publishPluginAction';
+import PublishToolAction from './publishToolAction';
 
 export default function resolveDocumentActions(props) {
+  // Tools need to fetch their readmeURLs before publishing, so they have their own publish action
   if (props.type === 'contribution.tool') {
     return [
-      PublishPluginAction,
+      PublishToolAction,
       ...defaultResolve(props).filter((action) => action.name !== 'PublishAction'),
     ];
   }
+  // Contribution documents need a distinct publish action for curatedContribution creation
   if (props.type.includes('contribution.')) {
     return [
       PublishContributionAction,
       ...defaultResolve(props).filter((action) => action.name !== 'PublishAction'),
     ];
   }
+  // Non-deletable documents
   if (
-    (props.type === 'person' && window._sanityUser?.provider === 'external') ||
+    props.type === 'person' ||
     props.type === 'taxonomy.contributionType'
   ) {
     return [

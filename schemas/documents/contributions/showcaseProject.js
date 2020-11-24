0b13387fe9d72c1c3ec0fd60c04bbe59406b606d
Henrique Doro <opensource@hdoro.dev>
2020-11-24 18:22:52 -0300

    Add social preview to contributions

--- schemas/documents/contributions/showcaseProject.js
+++ schemas/documents/contributions/showcaseProject.js
@@ -31,7 +31,7 @@ export default {
       name: 'description',
       title: 'Headline / short description for the project',
       description:
-        'Use this space to explain briefly what the project is. You\'ll have room for details in the in-depth explanation field below :)',
+        "Use this space to explain briefly what the project is. You'll have room for details in the in-depth explanation field below :)",
       type: 'text',
       rows: 1,
     },
@@ -39,7 +39,7 @@ export default {
       name: 'url',
       type: 'url',
       title: 'URL to access it',
-      description: 'If you don\'t have a public URL, feel free to leave this empty',
+      description: "If you don't have a public URL, feel free to leave this empty",
     },
     {
       name: 'slug',
@@ -69,27 +69,17 @@ export default {
       ],
     },
     {
-      name: 'studioScreenshots',
-      type: 'array',
-      title: 'Sanity Studio Screenshots',
-      description:
-        'Some suggestions for what to screenshot: your desk structure; the dashboard and other tools installed; your most interesting schema, etc.', // @todo: find instructions for how to take the best screenshot
-      of: [
-        {
-          type: 'studioImage',
-        },
-      ],
-    },
-    {
       name: 'projectScreenshots',
       type: 'array',
       title: 'Screenshots of the project',
+      description:
+        "The first of these screenshots will be used in the hero and sharing image of this project's page",
       // description: 'Here’s how to take a nice screenshot', // @todo: find instructions for how to take the best screenshot
       of: [
         {
           type: 'image',
           options: {
-            hotspot: true
+            hotspot: true,
           },
           fields: [
             {
@@ -115,6 +105,18 @@ export default {
       ],
     },
     {
+      name: 'studioScreenshots',
+      type: 'array',
+      title: 'Sanity Studio Screenshots',
+      description:
+        "Some suggestions for what to screenshot: your desk structure; the dashboard and other tools installed; your most interesting schema, etc. If you didn't include any project screenshot above, the first of these screenshots will be used in the hero and sharing image of this project's page", // @todo: find instructions for how to take the best screenshot
+      of: [
+        {
+          type: 'studioImage',
+        },
+      ],
+    },
+    {
       title: 'In-depth explanation of the project',
       name: 'body',
       type: 'array',
@@ -124,49 +126,56 @@ export default {
           styles: [{title: 'Normal', value: 'normal'}],
         },
       ],
-      description: 'Here you can talk about the challenges, the solutions you came up with, how did you choose the tech, etc.',
+      description:
+        'Here you can talk about the challenges, the solutions you came up with, how did you choose the tech, etc.',
     },
     {
       name: 'categories',
       title: 'Category(ies)',
-      description: 'Get in touch if you don\'t find the category you were looking for',
+      description: "Get in touch if you don't find the category you were looking for",
       // @TODO: description & maybe input component that allows to submit new taxonomy draft inline
       type: 'array',
-      of: [{
-        type: 'reference',
-        title: 'Reference to showcase categories',
-        to: [{ type: "taxonomy.category" }],
-        options: {
-          filter: "$type in applicableTo",
-          filterParams: {
-            type: "contribution.showcaseProject"
-          }
-        }
-      }]
+      of: [
+        {
+          type: 'reference',
+          title: 'Reference to showcase categories',
+          to: [{type: 'taxonomy.category'}],
+          options: {
+            filter: '$type in applicableTo',
+            filterParams: {
+              type: 'contribution.showcaseProject',
+            },
+          },
+        },
+      ],
     },
     {
       name: 'frameworks',
       title: 'Framework(s) / tech you used when creating this',
-      description: 'Get in touch if you don\'t find the tech you were looking for',
+      description: "Get in touch if you don't find the tech you were looking for",
       // @TODO: description & maybe input component that allows to submit new taxonomy draft inline
       type: 'array',
-      of: [{
-        type: 'reference',
-        title: 'Reference to framework',
-        to: [{ type: "taxonomy.framework" }],
-      }]
+      of: [
+        {
+          type: 'reference',
+          title: 'Reference to framework',
+          to: [{type: 'taxonomy.framework'}],
+        },
+      ],
     },
     {
       name: 'integrations',
       title: 'Services you integrated with',
-      description: 'Get in touch if you don\'t find the one(s) you were looking for',
+      description: "Get in touch if you don't find the one(s) you were looking for",
       // @TODO: description & maybe input component that allows to submit new taxonomy draft inline
       type: 'array',
-      of: [{
-        type: 'reference',
-        title: 'Reference to integration',
-        to: [{ type: "taxonomy.integration" }],
-      }]
+      of: [
+        {
+          type: 'reference',
+          title: 'Reference to integration',
+          to: [{type: 'taxonomy.integration'}],
+        },
+      ],
     },
   ],
   preview: {

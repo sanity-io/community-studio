a66ff296eb5bc221d2ed1dbd40b5d3fd81980a07
Henrique Doro <opensource@hdoro.dev>
2021-02-23 11:51:31 -0300

    New globalSettings file for site-wide configuration

---
+++ schemas/documents/globalSettings.js
@@ -0,0 +1,57 @@
+const scriptBodyType = {
+  type: 'array',
+  of: [
+    {
+      title: 'Block',
+      type: 'block',
+      styles: [
+        {title: 'Normal', value: 'normal'},
+        {title: 'Heading 2', value: 'h2'},
+        {title: 'Heading 3', value: 'h3'},
+        {title: 'Quote', value: 'blockquote'},
+      ],
+    },
+  ],
+  validation: (Rule) => Rule.required(),
+};
+
+export default {
+  name: 'globalSettings',
+  title: 'Global settings',
+  type: 'document',
+  fields: [
+    {
+      ...scriptBodyType,
+      name: 'answersConversionScript',
+      title: 'Conversion script for answers / tickets',
+    },
+    {
+      ...scriptBodyType,
+      name: 'guidesConversionScript',
+      title: 'Conversion script for guides',
+    },
+    {
+      name: 'guidesConversionIllustration',
+      title: "Illustration for guides' conversion script",
+      type: 'image',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'menuIntegrations',
+      title: 'Integrations in the global nav',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'taxonomy.framework'}, {type: 'taxonomy.integration'}]}],
+      validation: (Rule) => [
+        Rule.required().length(9).error('We need exactly 9 integrations for the menu.'),
+        Rule.unique(),
+      ],
+    },
+  ],
+  preview: {
+    prepare() {
+      return {
+        title: `Global settings`,
+      };
+    },
+  },
+};

2d765b233955faa25d055a56e186ead211a9872f
Henrique Doro <opensource@hdoro.dev>
2021-03-24 16:02:46 -0300

    Adding slug & color fields to techPartner

--- schemas/documents/techPartner.js
+++ schemas/documents/techPartner.js
@@ -1,111 +1,152 @@
 //Content type for Sanity partners
 
-import figure from "../objects/figure"
+import brandColorList from '../../src/utils/brandColorList';
+import PathInput from '../components/PathInput';
 
- 
-export default{
-    title:  "Technology Partner",
-    name: "techPartner",
-    type: "document",
+export default {
+  title: 'Technology Partner',
+  name: 'techPartner',
+  type: 'document',
 
-    fields: [
-      {
-
-        title: "Company Name",
-        name: "companyName",
-        type: "string",
+  fields: [
+    {
+      title: 'Company Name',
+      name: 'companyName',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'One-liner about the product',
+      name: 'oneLiner',
+      description: 'In a short sentence, what does this product offer?',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Relative address in the site',
+      description: 'Please avoid special characters, spaces and uppercase letters.',
+      type: 'slug',
+      inputComponent: PathInput,
+      options: {
+        basePath: 'sanity.io/partners',
+        source: 'companyName',
       },
-      {
-        title: 'One Liner',        
-        name: 'oneLiner',        
-        type: 'string'
-      },
-      {
-        title: 'Short Description',        
-        name: 'shortDesc',        
-        type: 'text'
-      },
-      {
-        title: 'Long Description',         
-        name: 'longDesc',        
-        type: 'array',         
-        of: [{type: 'block'}]
-      },
-      {
-        title: 'Partner Logos',
-        name: 'partnerLogos',
-        type: 'figure',
-        description: 'Ideally this image has a transparent background for use over other images or on non-white backgrounds.',
-      },
-      // Start references 
-      {
-        name: 'editors',
-        type: 'array',
-        title: '🖊️ Editor(s)',
-        description:
-          'Ask about context,to make appropiate description',
-        of: [
-          {
-            type: 'reference',
-            to: [{type: 'person'}],
-          },
-        ],
-      },
-      {
-        name: 'techProjects',
-        type: 'array',
-        title: 'Partner Project(s)',
-        description:
-          'Ask about context,to make appropiate description',
-        of: [
-          {
-            type: 'reference',
-            to: [{type: 'contribution.showcaseProject'}],
-            title: 'Partner Projects',
-          },
-        ],
-      },
-      {
-        name: 'techStarters',
-        type: 'array',
-        title: 'Partner Starter(s)',
-        description:
-          'Ask about context,to make appropiate description',
-        of: [
-          {
-            type: 'reference',
-            to: [{type: 'contribution.starter'}],
-            title: 'Partner Starters',
-          },
-        ],
-      },
-      {
-        name: 'techPlugins',
-        type: 'array',
-        title: 'Partner Plugin(s)',
-        description:
-          'Ask about context,to make appropiate description',
-        of: [
-          {
-            type: 'reference',
-            to: [{type: 'contribution.tool'}],
-            title: 'Partner Plugins',
-          },
-        ],
-      },
-      {
-        name: 'techSnippets',
-        type: 'array',
-        title: 'Partner Snippet(s)',
-        description:
-          'Ask about context,to make appropiate description',
-        of: [
-          {
-            type: 'reference',
-            to: [{type: 'contribution.schema'}],
-            title: 'Partner Snippets',
-          },
-        ],
-      },
-    ]
-  }
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: '👀 Hide this partner?',
+      name: 'hidden',
+      type: 'boolean',
+      description: 'Turn this on to stop this partnership from being seen while you work on it.',
+    },
+    {
+      title: 'Short Description',
+      description: 'Brief introduction of how Sanity and this product play together. Keep it short, 3 lines maximum.',
+      name: 'shortDesc',
+      type: 'text',
+      rows: 3,
+    },
+    {
+      title: 'Long Description',
+      name: 'longDesc',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      title: 'Logos',
+      name: 'partnerLogos',
+      type: 'figure',
+      description:
+        'Ideally this image has a transparent background for use over other images or on non-white backgrounds.',
+    },
+    {
+      name: 'color',
+      title: 'Brand color',
+      description: 'In some applications of the logo, we\'ll put a background with this color behind it.',
+      type: 'color',
+      validation: (Rule) => Rule.required(),
+    },
+    // Start references
+    {
+      title: 'Related taxonomy',
+      name: 'taxonomy',
+      type: 'reference',
+      to: [
+        {type: 'taxonomy.integration'},
+        {type: 'taxonomy.framework'},
+      ]
+    },
+    {
+      name: 'editors',
+      type: 'array',
+      title: '🖊️ Editor(s)',
+      description: 'People from the team who should have edit access to this document.',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'person'}],
+        },
+      ],
+    },
+    {
+      name: 'techProjects',
+      type: 'array',
+      title: 'Partner Project(s)',
+      description: 'Handpicked selection of the best community projects made with this product.',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'contribution.showcaseProject'}],
+          title: 'Partner Projects',
+        },
+      ],
+    },
+    {
+      name: 'techStarters',
+      type: 'array',
+      title: 'Partner Starter(s)',
+      description: 'Handpicked selection of the starters made with this product.',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'contribution.starter'}],
+          title: 'Partner Starters',
+        },
+      ],
+    },
+    {
+      name: 'techPlugins',
+      type: 'array',
+      title: 'Partner Plugin(s)',
+      description: 'Handpicked selection of plugins made with this product.',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'contribution.tool'}],
+          title: 'Partner Plugins',
+        },
+      ],
+    },
+    {
+      name: 'techSnippets',
+      type: 'array',
+      title: 'Partner Snippet(s)',
+      description: 'Handpicked selection of snippets made with this product.',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'contribution.schema'}],
+          title: 'Partner Snippets',
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'companyName',
+      subtitle: 'oneLiner',
+      media: 'partnerLogos.logoTransparent',
+    },
+  },
+};

929bfbc1c24e8306208abdb46673095aa89977b9
Knut Melvær <knut.melvaer@gmail.com>
2022-01-07 18:10:55 -0800

    update current user

--- schemas/components/functions/index.js
+++ schemas/components/functions/index.js
@@ -2,12 +2,12 @@ import userStore from 'part:@sanity/base/user'
 import client from 'part:@sanity/base/client'
 
 export const getCurrentUser = () => {
-  return new Promise(function(res, rej) {
-    userStore.currentUser.subscribe(event => {
+  return new Promise(function (res, rej) {
+    userStore.me.subscribe(event => {
       client.fetch(`*[_type == "person" && sanityId == $userId]`,
-      { userId: event.user.id }).then(result => {
-        res(result[0])
-      })
+        { userId: event.user.id }).then(result => {
+          res(result[0])
+        })
     })
   })
 }

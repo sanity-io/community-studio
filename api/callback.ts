7a5e89b74caef29a9ad6ed74b9ffa11a9fa5d967
Knut Melvær <knut.melvaer@gmail.com>
2022-01-10 10:36:04 -0800

    replace node-fetch with axios

--- api/callback.ts
+++ api/callback.ts
@@ -1,11 +1,11 @@
-import { createHash } from 'crypto'
-import fetch from 'node-fetch'
-import sanityClient from '@sanity/client'
-import url from 'url'
+import {createHash} from 'crypto';
+import fetch from 'axios';
+import sanityClient from '@sanity/client';
+import url from 'url';
 
-import { OAuth2 } from '../src/utils/oauth'
-import agentGroup from '../src/roles/agent'
-import contributorGroup from '../src/roles/contributor'
+import {OAuth2} from '../src/utils/oauth';
+import agentGroup from '../src/roles/agent';
+import contributorGroup from '../src/roles/contributor';
 
 const sanityOAuth2 = OAuth2({
   clientId: process.env.SANITY_OAUTH_CLIENT_ID,
@@ -13,38 +13,34 @@ const sanityOAuth2 = OAuth2({
   authorizationUrl: process.env.SANITY_OAUTH_AUTH_URL,
   tokenUrl: process.env.SANITY_OAUTH_TOKEN_URL,
   redirectUri: process.env.SANITY_OAUTH_CALLBACK_URL,
-  useBasicAuthorizationHeader: false
-})
+  useBasicAuthorizationHeader: false,
+});
 
-const opts = {}
+const opts = {};
 
-const projectId = process.env.SANITY_PROJECT_ID
-const createSessionToken = process.env.SANITY_CREATE_SESSION_TOKEN
+const projectId = process.env.SANITY_PROJECT_ID;
+const createSessionToken = process.env.SANITY_CREATE_SESSION_TOKEN;
 
 const client = sanityClient({
   projectId,
   dataset: process.env.SANITY_DATASET,
   token: process.env.SANITY_WRITE_TOKEN,
-  useCdn: false
-})
+  useCdn: false,
+});
 
 const sessionClient = client.config({
-  token: createSessionToken
-})
+  token: createSessionToken,
+});
 
 const userIdFromEmail = (email: string) => {
-  const hash = createHash('md5')
-    .update(email)
-    .digest('hex')
-  return `e-${hash}`
-}
+  const hash = createHash('md5').update(email).digest('hex');
+  return `e-${hash}`;
+};
 
 const userFromProfile = (user, role) => {
-  const sessionExpires = new Date(
-    new Date().getTime() + 7 * 24 * 60 * 60 * 1000
-  ).toISOString()
+  const sessionExpires = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toISOString();
 
-  const userId = userIdFromEmail(user.email)
+  const userId = userIdFromEmail(user.email);
 
   return {
     userId,
@@ -53,111 +49,101 @@ const userFromProfile = (user, role) => {
     userImage: user.profileImage || `https://www.gravatar.com/avatar/${userId.slice(2)}?d=retro`,
     userRole: role,
     sessionExpires,
-    sessionLabel: role === 'agent'
-      ? 'SSO'
-      : 'Community'
-  }
-}
+    sessionLabel: role === 'agent' ? 'SSO' : 'Community',
+  };
+};
 
 export default async function callback(req, res) {
-  const urlObj = url.parse(req.url, true)
+  const urlObj = url.parse(req.url, true);
 
-  const cookiesObj = Object
-    .fromEntries(req.headers.cookie.split('; ')
-    .map(v => v.split(/=(.+)/)))
+  const cookiesObj = Object.fromEntries(
+    req.headers.cookie.split('; ').map((v) => v.split(/=(.+)/))
+  );
 
   if (urlObj.query.code && urlObj.query.state) {
-    const authCode = urlObj.query.code
+    const authCode = urlObj.query.code;
 
-    const queryState = Buffer
-      .from(decodeURIComponent(urlObj.query.state), 'base64')
-      .toString()
-    const cookieState = decodeURIComponent(cookiesObj.state)
+    const queryState = Buffer.from(decodeURIComponent(urlObj.query.state), 'base64').toString();
+    const cookieState = decodeURIComponent(cookiesObj.state);
 
     // Verify nonce state parameters match
     if (!queryState || queryState !== cookieState) {
       res.writeHead(302, {
-        Location: `https://community.sanity.tools/?ref=cb`
-      })
-      res.end()
+        Location: `https://community.sanity.tools/?ref=cb`,
+      });
+      res.end();
     }
 
     try {
-      const token = await sanityOAuth2.getAccessToken(authCode, opts)
+      const token = await sanityOAuth2.getAccessToken(authCode, opts);
       const profile = await fetch('https://api.sanity.io/v1/users/me', {
-          headers: {
-            Authorization: `${token.token_type} ${token.access_token}`
-          }
-        })
-        .then(res => res.json())
+        headers: {
+          Authorization: `${token.token_type} ${token.access_token}`,
+        },
+      }).then((res) => res.data);
 
       const role =
         (profile.provider === 'google' && profile.email.endsWith('@sanity.io')) ||
         (profile.provider === 'github' && profile.email === process.env.SANITY_TOKEN_A) ||
         (profile.provider === 'github' && profile.email === process.env.SANITY_TOKEN_B)
           ? 'agent'
-          : 'editor'
+          : 'editor';
 
-      const user = userFromProfile(profile, role)
+      const user = userFromProfile(profile, role);
 
       const userDoc = {
         _id: user.userId,
         _type: 'person',
         name: user.userFullName,
-        imageUrl: user.userImage
-      }
+        imageUrl: user.userImage,
+      };
 
-      await client
-        .create(userDoc)
-        .catch(err => {
-          if (err.statusCode === 409) {
-            return client.patch(userDoc._id)
+      await client.create(userDoc).catch((err) => {
+        if (err.statusCode === 409) {
+          return client
+            .patch(userDoc._id)
             .set({
-              imageUrl: userDoc.imageUrl
+              imageUrl: userDoc.imageUrl,
             })
-            .commit()
-          } else {
-            throw err
-          }
-        })
+            .commit();
+        } else {
+          throw err;
+        }
+      });
 
       await sessionClient
-        .createIfNotExists(role === 'agent'
-          ? agentGroup
-          : contributorGroup
-        )
+        .createIfNotExists(role === 'agent' ? agentGroup : contributorGroup)
         .then((group) => {
           if (!(group.members || []).includes(user.userId)) {
             return sessionClient
               .patch(group._id)
               .setIfMissing({members: []})
               .append('members', [user.userId])
-              .commit()
+              .commit();
           }
-        })
+        });
 
       const endUserClaimUrl = await sessionClient
         .request({
           uri: '/auth/thirdParty/session',
           method: 'POST',
           json: true,
-          body: user
+          body: user,
         })
-        .then(result => {
-          return result.endUserClaimUrl
+        .then((result) => {
+          return result.endUserClaimUrl;
         })
-        .catch(err => {
-          throw err
-        })
+        .catch((err) => {
+          throw err;
+        });
 
       res.writeHead(302, {
-        Location: `${endUserClaimUrl}?origin=${process.env.SANITY_STUDIO_URL}`
-      })
-      res.end()
-
+        Location: `${endUserClaimUrl}?origin=${process.env.SANITY_STUDIO_URL}`,
+      });
+      res.end();
     } catch (error) {
-      console.error(error)
-      res.status(error.status || 400).end(error.message)
+      console.error(error);
+      res.status(error.status || 400).end(error.message);
     }
   }
 }

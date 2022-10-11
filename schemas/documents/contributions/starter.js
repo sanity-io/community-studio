8f5ddd702d1b8d1cbfabcbd22725469685a83037
RitaDias <rita@sanity.io>
2022-10-11 10:33:21 +0200

    feat: add studio version to starters schema (#94)
    * feat: add studio version
    
    * feat: update declaration for node engine

--- schemas/documents/contributions/starter.js
+++ schemas/documents/contributions/starter.js
@@ -148,6 +148,22 @@ export default {
       ],
     },
     {
+      name: 'studioVersion',
+      title: 'Studio version',
+      type: 'number',
+      description: 'What Sanity Studio version was this starter was built for.',
+      initialValue: -1,
+      options: {
+        layout: 'radio',
+        direction: 'horizontal',
+        list: [
+          {value: -1, title: 'N/A'},
+          {value: 2, title: 'Studio v2'},
+          {value: 3, title: 'Studio v3'},
+        ],
+      },
+    },
+    {
       name: 'slug',
       type: 'slug',
       title: 'Relative address in the community site',

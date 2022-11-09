1d09cb035ffee4014d0f27975961ea52dfdc4c09
Racheal Pennell <racheal.larimer@gmail.com>
2022-11-09 10:38:08 -0800

    fix structure not responding to changes in user profile tags

--- parts/supportStructure.js
+++ parts/supportStructure.js
@@ -19,125 +19,124 @@ const client = sanityClient.withConfig({apiVersion: '2022-10-31'});
 
 const weekThreshold = formatISO(subHours(new Date(), 168));
 
-const getSupportStructure = () => {
-  return S.listItem()
+const getSupportStructure = () =>
+  S.listItem()
     .title('Support')
     .icon(() => <EnvelopeIcon />)
-    .child(async () => {
-      const user = await userStore.getCurrentUser().then(
-        async (user) =>
-          await client.fetch(
-            `*[_type == 'person' && (_id == $id || _id == 'drafts.' + $id)][0]{ ..., 'tags': tags[]._ref, 'savedTickets': savedTickets[]._ref}`,
-            {
-              id: user.id,
-            }
+    .child(async () =>
+      userStore.getCurrentUser().then((user) =>
+        documentStore
+          .listenQuery(
+            `*[_type == 'person' && (_id == $id || _id == 'drafts.' + $id)] | order(_updatedAt desc)[0]{ ..., 'tags': tags[]._ref, 'savedTickets': savedTickets[]._ref}`,
+            {id: user.id},
+            {apiVersion: '2021-10-21'}
           )
-      );
-      console.log(user);
-
-      return S.list()
-        .title('Support')
-        .items([
-          S.listItem()
-            .title('Your Feed')
-            .icon(ActivityIcon)
-            .child(
-              S.documentList()
-                .title('Your Feed')
-                .filter(
-                  `_type == 'ticket' && count((tags[]._ref)[@ in $tags]) > 0 && _createdAt > $weekThreshold`
-                )
-                .params({tags: user.tags, weekThreshold})
-                .apiVersion('v2021-06-07')
-                .menuItems([...S.documentTypeList('ticket').getMenuItems()])
-            ),
-          S.listItem()
-            .icon(UserIcon)
-            .title('Your Tickets')
-            .child(() =>
-              documentStore
-                .listenQuery(
-                  `*[_type == 'ticket' && $id in thread[].author.slackId]`,
-                  {
-                    id: user.slackId,
-                  },
-                  {apiVersion: '2021-10-21'}
-                )
-                .pipe(
-                  map((tickets) =>
-                    S.list()
-                      .title('Your Feed')
-                      .items(
-                        tickets.map((ticket) =>
-                          S.documentListItem().id(ticket._id).schemaType(ticket._type)
-                        )
-                      )
-                  )
-                )
-            ),
-          S.listItem()
-            .title('Saved Tickets')
-            .icon(HeartIcon)
-            .child(
-              S.documentList()
-                .title('Your Feed')
-                .filter(`_type == 'ticket' && _id in $savedTickets`)
-                .params({savedTickets: user.savedTickets})
-                .apiVersion('v2021-06-07')
-                .menuItems([...S.documentTypeList('ticket').getMenuItems()])
-            ),
-          S.divider(),
-          S.listItem()
-            .title('All Tickets')
-            .icon(() => <EnvelopeIcon />)
-            .child(
+          .pipe(
+            map((user) =>
               S.list()
-                .title('All Tickets')
+                .title('Support')
                 .items([
                   S.listItem()
-                    .title('New')
-                    .icon(StarIcon)
+                    .title('Your Feed')
+                    .icon(ActivityIcon)
                     .child(
                       S.documentList()
-                        .title('New Tickets')
-                        .filter(`_type == 'ticket' && _createdAt > $weekThreshold`)
-                        .params({weekThreshold})
-                    ),
-                  S.listItem()
-                    .title('Recently Resolved')
-                    .icon(CheckmarkCircleIcon)
-                    .child(
-                      S.documentList()
-                        .title('New Tickets')
+                        .title('Your Feed')
                         .filter(
-                          `_type == 'ticket' && _createdAt > $weekThreshold && status == 'resolved'`
+                          `_type == 'ticket' && count((tags[]._ref)[@ in $tags]) > 0 && _createdAt > $weekThreshold`
                         )
-                        .params({weekThreshold})
+                        .params({tags: user.tags, weekThreshold})
+                        .apiVersion('v2021-06-07')
+                        .menuItems([...S.documentTypeList('ticket').getMenuItems()])
                     ),
-                  ,
                   S.listItem()
-                    .title('Tickets by Tag')
-                    .icon(TagIcon)
+                    .icon(UserIcon)
+                    .title('Your Tickets')
                     .child(
-                      S.documentTypeList('tag')
-                        .title('Tickets by Tag')
-                        .defaultOrdering([{field: 'title', direction: 'asc'}])
-                        .child((tagId) =>
-                          S.documentList()
-                            .title('Tickets')
-                            .filter(`_type == 'ticket' && $tagId in tags[]._ref`)
-                            .params({tagId})
-                        )
+                      async () =>
+                        await client
+                          .fetch(
+                            `*[_type == 'ticket' && $id in thread[].author.slackId]`,
+                            {
+                              id: user.slackId,
+                            },
+                            {apiVersion: '2021-10-21'}
+                          )
+                          .then((tickets) =>
+                            S.list()
+                              .title('Your Feed')
+                              .items(
+                                tickets.map((ticket) =>
+                                  S.documentListItem().id(ticket._id).schemaType(ticket._type)
+                                )
+                              )
+                          )
+                    ),
+                  S.listItem()
+                    .title('Saved Tickets')
+                    .icon(HeartIcon)
+                    .child(
+                      S.documentList()
+                        .title('Your Feed')
+                        .filter(`_type == 'ticket' && _id in $savedTickets`)
+                        .params({savedTickets: user.savedTickets})
+                        .apiVersion('v2021-06-07')
+                        .menuItems([...S.documentTypeList('ticket').getMenuItems()])
                     ),
                   S.divider(),
                   S.listItem()
                     .title('All Tickets')
                     .icon(() => <EnvelopeIcon />)
-                    .child(S.documentTypeList('ticket')),
+                    .child(
+                      S.list()
+                        .title('All Tickets')
+                        .items([
+                          S.listItem()
+                            .title('New')
+                            .icon(StarIcon)
+                            .child(
+                              S.documentList()
+                                .title('New Tickets')
+                                .filter(`_type == 'ticket' && _createdAt > $weekThreshold`)
+                                .params({weekThreshold})
+                            ),
+                          S.listItem()
+                            .title('Recently Resolved')
+                            .icon(CheckmarkCircleIcon)
+                            .child(
+                              S.documentList()
+                                .title('New Tickets')
+                                .filter(
+                                  `_type == 'ticket' && _createdAt > $weekThreshold && status == 'resolved'`
+                                )
+                                .params({weekThreshold})
+                            ),
+                          ,
+                          S.listItem()
+                            .title('Tickets by Tag')
+                            .icon(TagIcon)
+                            .child(
+                              S.documentTypeList('tag')
+                                .title('Tickets by Tag')
+                                .defaultOrdering([{field: 'title', direction: 'asc'}])
+                                .child((tagId) =>
+                                  S.documentList()
+                                    .title('Tickets')
+                                    .filter(`_type == 'ticket' && $tagId in tags[]._ref`)
+                                    .params({tagId})
+                                )
+                            ),
+                          S.divider(),
+                          S.listItem()
+                            .title('All Tickets')
+                            .icon(() => <EnvelopeIcon />)
+                            .child(S.documentTypeList('ticket')),
+                        ])
+                    ),
                 ])
-            ),
-        ]);
-    });
-};
+            )
+          )
+      )
+    );
 
 export default getSupportStructure;

e0f43f74bd49053750a8a01e279a69df026ef9c6
Peter <627189+p10e@users.noreply.github.com>
2022-11-14 18:58:05 +0100

    API updates to auto-record Community tickets (#99)
    * feat: adds Studio version fields
    
    * fix: casing of "Studio v3" fixed
    
    * upgrade studio
    
    * Add vercel deploy link as starter options (#92) (#93)
    
    * feat: add vercel jumpstart type
    
    * fix: add preview for vercel jumpstart
    
    * refactor: change jumpstart to deploy
    
    * refactor: capitalise vercel
    
    * refactor: Update sanity jumpstart name schemas/documents/contributions/starter.js
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: Update sanity jumpstart name schemas/documents/contributions/starter.js
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: capitalise github schemas/documents/contributions/starter.js
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: capitalise Vercel
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: change version from github to sanitycreate
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: change version from github to sanitycreate
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: change version from github to sanitycreate
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: have repoid always available
    
    * refactor: update order for doc
    
    * refactor: fix issues with validation
    
    * refactor: switch jumpstartstarttype to deploymenttype
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: update title
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: update casing & copy
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * Apply suggestions from code review
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * fix(starters): fix alternative path for repo id validation
    
    * release staging -> main (#95)
    
    * Add vercel deploy link as starter options (#92)
    
    * feat: add vercel jumpstart type
    
    * fix: add preview for vercel jumpstart
    
    * refactor: change jumpstart to deploy
    
    * refactor: capitalise vercel
    
    * refactor: Update sanity jumpstart name schemas/documents/contributions/starter.js
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: Update sanity jumpstart name schemas/documents/contributions/starter.js
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: capitalise github schemas/documents/contributions/starter.js
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: capitalise Vercel
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: change version from github to sanitycreate
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: change version from github to sanitycreate
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: change version from github to sanitycreate
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: have repoid always available
    
    * refactor: update order for doc
    
    * refactor: fix issues with validation
    
    * refactor: switch jumpstartstarttype to deploymenttype
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: update title
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * refactor: update casing & copy
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * Apply suggestions from code review
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * fix(starters): fix alternative path for repo id validation
    
    * feat: add studio version to starters schema (#94)
    
    * feat: add studio version
    
    * feat: update declaration for node engine
    
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>
    
    * Add enqueue event handling
    
    * Remove unused secrets
    
    * Add process event handling
    
    * Update message and reaction handlers
    
    Co-authored-by: Cody Olsen <stipsan@gmail.com>
    Co-authored-by: Knut Melvær <knut@sanity.io>
    Co-authored-by: RitaDias <rita@sanity.io>
    Co-authored-by: Fred Carlsen <fred@sjelfull.no>

---
+++ src/createEnqueueEventHandler.ts
@@ -0,0 +1,63 @@
+import {VercelRequest, VercelResponse} from '@vercel/node';
+import {PubSub} from '@google-cloud/pubsub';
+import {ExternalDataSourceName, externalDataSourceNames} from '../src/types';
+import isSlackDataSource from '../src/utils/is-slack-data-source';
+
+const pubSub = new PubSub({
+  projectId: process.env.GCP_PROJECT_ID,
+  credentials: {
+    private_key: process.env.GCP_PRIVATE_KEY,
+    client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL_ADDRESS,
+  },
+});
+
+export default function createEnqueueEventHandler(overrideSourceName?: ExternalDataSourceName) {
+  return async function handler(request: VercelRequest, response: VercelResponse) {
+    const [sourceName] = overrideSourceName
+      ? [overrideSourceName]
+      : ([] as ExternalDataSourceName[]).concat(request.query.sourceName as ExternalDataSourceName);
+
+    if (request.method !== 'POST') {
+      response.status(405).json({
+        success: false,
+        message: 'Method not allowed',
+      });
+
+      return;
+    }
+
+    if (!externalDataSourceNames.includes(sourceName)) {
+      response.status(422).json({
+        success: false,
+      });
+
+      return;
+    }
+
+    // Handle Slack's verification challenge.
+    if (isSlackDataSource(sourceName) && request.body.challenge) {
+      response.json({
+        body: request.body.challenge,
+      });
+
+      return;
+    }
+
+    const topic = pubSub.topic(sourceName);
+
+    try {
+      const messageId = await topic.publishMessage({
+        json: request.body,
+      });
+
+      response.json({
+        success: true,
+        messageId,
+      });
+    } catch (error) {
+      response.status(500).json({
+        success: false,
+      });
+    }
+  };
+}
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Secrets {
+  SANITY_PROJECT_ID: string;
+  SANITY_DATASET: string;
+  SANITY_WRITE_TOKEN: string;
+  SLACK_BOT_USER_TOKEN: string;
+  EMAIL_DOMAIN: string;
+}
+
+export const externalDataSourceNames = ['slack-community'] as const;
+
+export type ExternalDataSourceName = typeof externalDataSourceNames[number];

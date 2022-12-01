50d1577855a0bf434ca83187f5410cf2b77d5df8
Racheal Pennell <racheal.larimer@gmail.com>
2022-12-01 11:22:38 -0800

    add button for saving tickets to ticket schema

--- schemas/components/SaveThreadButton.js
+++ schemas/components/SaveThreadButton.js
@@ -1,9 +1,20 @@
 import React, {forwardRef, useCallback, useState, useEffect} from 'react';
 import {withDocument} from 'part:@sanity/form-builder';
+import {Flex, Button, Spinner, Card} from '@sanity/ui';
 import {FormField} from '@sanity/base/components';
 import client from 'part:@sanity/base/client';
+import {useId} from '@reach/auto-id';
+import {PinRemovedIcon, PinIcon} from '@sanity/icons';
 
 const sanityClient = client.withConfig({apiVersion: '2022-11-30'});
+
+const SpinnerIcon = (
+  <>
+    <Flex align="center" height="fill" justify="center">
+      <Spinner muted />
+    </Flex>
+  </>
+);
 
 const SaveTicketButton = forwardRef((props, ref) => {
   const [isSavedTicket, setIsSavedTicket] = useState();
@@ -22,16 +33,32 @@ const SaveTicketButton = forwardRef((props, ref) => {
     document,
   } = props;
 
-  const refId = document._id.startsWith('drafts.') ? document._id.slice(7) : document._id;
+  const refId = document._id?.startsWith('drafts.') ? document._id.slice(7) : document._id;
 
   const handleClick = async () => {
-    setIsPatching(true);
-    await sanityClient
-      .patch(profileId)
-      .setIfMissing({savedTickets: []})
-      .append('savedTickets', [{_type: 'reference', _ref: refId}])
-      .commit({autoGenerateArrayKeys: true})
-      .then(() => setIsSavedTicket(true));
+    if (isSavedTicket) {
+      const ticket = [`savedTickets[_ref=="${refId}"]`];
+      setIsPatching(true);
+      await sanityClient
+        .patch(profileId)
+        .unset(ticket)
+        .commit()
+        .then(() => {
+          setIsSavedTicket(false);
+          setIsPatching(false);
+        });
+    } else {
+      setIsPatching(true);
+      await sanityClient
+        .patch(profileId)
+        .setIfMissing({savedTickets: []})
+        .append('savedTickets', [{_type: 'reference', _ref: refId}])
+        .commit({autoGenerateArrayKeys: true})
+        .then(() => {
+          setIsSavedTicket(true);
+          setIsPatching(false);
+        });
+    }
   };
 
   useEffect(() => {
@@ -52,8 +79,32 @@ const SaveTicketButton = forwardRef((props, ref) => {
       });
   }, []);
 
-  console.log('saved', isSavedTicket);
-  return <button onClick={handleClick}>{isSavedTicket ? 'Unsave Ticket' : 'Save Ticket'}</button>;
+  const inputId = useId();
+
+  return (
+    <FormField
+      compareValue={compareValue}
+      __unstable_markers={markers}
+      __unstable_presence={presence}
+      inputId={inputId}
+    >
+      <Flex justify="flex-end">
+        <Button
+          onFocus={onFocus}
+          onBlur={onBlur}
+          ref={ref}
+          fontSize={1}
+          onClick={handleClick}
+          tone={isSavedTicket ? 'brand' : 'positive'}
+          type="button"
+          icon={isPatching ? SpinnerIcon : isSavedTicket ? PinRemovedIcon : PinIcon}
+          text={
+            isPatching ? null : isSavedTicket ? 'Remove From Saved Tickets' : 'Add to Saved Tickets'
+          }
+        />
+      </Flex>
+    </FormField>
+  );
 });
 
 export default withDocument(SaveTicketButton);

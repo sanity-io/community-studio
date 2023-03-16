26bf49e434c5c798d3ae6a180553e173d0c466ae
Geoff Ball <geoff@gball.dev>
2023-03-16 15:21:32 -0600

    Migrate Google Maps Input config to v3

--- sanity.config.js
+++ sanity.config.js
@@ -1,15 +1,16 @@
-import {defineConfig} from 'sanity';
-import {deskTool} from 'sanity/desk';
-import {visionTool} from '@sanity/vision';
-import {dashboardTool} from '@sanity/dashboard';
-import {schemaTypes} from './schemas';
-import {structure} from './plugins/desk';
-import {colorInput} from '@sanity/color-input';
-import {markdownSchema} from 'sanity-plugin-markdown';
-import {codeInput} from '@sanity/code-input';
-import {getDefaultDocumentNode} from './plugins/desk/defaultDocumentNode';
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { dashboardTool } from '@sanity/dashboard';
+import { schemaTypes } from './schemas';
+import { structure } from './plugins/desk';
+import { colorInput } from '@sanity/color-input';
+import { markdownSchema } from 'sanity-plugin-markdown';
+import { codeInput } from '@sanity/code-input';
+import { getDefaultDocumentNode } from './plugins/desk/defaultDocumentNode';
 import dashboardConfig from './plugins/dashboardConfig';
-import {resolveProductionUrl} from './plugins/resolveProductionUrl';
+import { googleMapsInput } from '@sanity/google-maps-input';
+import { resolveProductionUrl } from './plugins/resolveProductionUrl';
 //import {resolveDocumentActions} from './plugins/actions';
 //import newDocumentStructure from './plugins/newDocumentStructure';
 //import initialValueTemplates from './plugins/initialValueTemplates';
@@ -26,11 +27,19 @@ export default defineConfig({
       structure,
       defaultDocumentNode: getDefaultDocumentNode,
     }),
-    visionTool({defaultApiVersion: '2021-10-21'}),
+    visionTool({ defaultApiVersion: '2021-10-21' }),
     colorInput(),
     markdownSchema(),
     codeInput(),
     dashboardTool(dashboardConfig),
+    googleMapsInput({
+      apiKey: "AIzaSyCB5AUdHPHaA-5jjVNrRp1sF4RRFQNqkHU",
+      defaultZoom: 11,
+      defaultLocation: {
+        lat: 40.7058254,
+        lng: -74.1180863
+      },
+    }),
   ],
   document: {
     productionUrl: resolveProductionUrl,

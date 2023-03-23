72a2880e99003b8a897240fd6f0aeb898afeba31
Ash <ash@sanity.io>
2023-03-23 17:13:22 +0000

    feat: add `IntentLinkCard` component

---
+++ src/components/IntentLinkCard.tsx
@@ -0,0 +1,16 @@
+import {ComponentType, ComponentProps} from 'react';
+import {useIntentLink} from 'sanity/router';
+import {Card} from '@sanity/ui';
+
+interface IntentLinkProps extends ComponentProps<typeof Card> {
+  link: Parameters<typeof useIntentLink>[0];
+}
+
+/**
+ * Render a Sanity UI Card component as an intent link.
+ */
+const IntentLinkCard: ComponentType<IntentLinkProps> = ({link, ...props}) => (
+  <Card as="a" {...props} {...useIntentLink(link)} />
+);
+
+export default IntentLinkCard;

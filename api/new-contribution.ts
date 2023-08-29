893264c94f44880fc95ecae99cd569044a91484e
Knut Melvær <knut@sanity.io>
2023-08-29 13:34:48 +0200

    add webhook validation

--- api/new-contribution.ts
+++ api/new-contribution.ts
@@ -3,12 +3,28 @@ import axios from 'axios';
 import OpenAI from 'openai';
 import toMarkdown from '@sanity/block-content-to-markdown';
 import {writeClient, readClient} from './curate-contribution';
+import {isValidSignature, SIGNATURE_HEADER_NAME} from '@sanity/webhook';
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+async function readBody(readable: VercelRequest) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString('utf8');
+}
 
 const slackWebhookUrl = process.env.SPAM_RATING_SLACK_WEBHOOK_URL;
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
+const webhookSecret = process.env.SANITY_WEBHOOK_SECRET;
 const TOKEN_LIMIT = process.env.OPENAI_TOKEN_LIMIT || 4097;
 const SUBMIT_TO_OPENAI = true;
 const WRITE_TO_SANITY = false;
@@ -71,7 +87,7 @@ async function getSpamScore(title: string, body: string, threshold: number, toke
         if (!SUBMIT_TO_OPENAI) {
           console.log({role: 'user', content: `title: ${title}\npart of body: ${subParagraph}`});
         }
-        
+
         if (SUBMIT_TO_OPENAI) {
           const chatCompletion = await openai.chat.completions.create({
             messages: [
@@ -100,7 +116,7 @@ async function getSpamScore(title: string, body: string, threshold: number, toke
       } catch (e) {
         console.log(e);
 
-        throw e
+        throw e;
       }
     }
   }
@@ -108,6 +124,15 @@ async function getSpamScore(title: string, body: string, threshold: number, toke
 }
 
 export default async function (req: VercelRequest, res: VercelResponse) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+  const body = await readBody(req);
+  // Only run in environments with a webhook secret. This is to prevent running this function in development.
+  if (webhookSecret) {
+    if (!isValidSignature(body, signature, webhookSecret)) {
+      res.status(401).json({success: false, message: 'Invalid signature'});
+      return;
+    }
+  }
   const document = req.body;
 
   const title = document?.title;
@@ -133,7 +158,9 @@ export default async function (req: VercelRequest, res: VercelResponse) {
 
   if (POST_TO_SLACK) {
     const slackMessage = {
-      text: `New contribution received: ${title}.\nSpam Rating: ${rating}\nReasons: ${reasons.join(', ')}`,
+      text: `New contribution received: ${title}.\nSpam Rating: ${rating}\nReasons: ${reasons.join(
+        ', '
+      )}`,
     };
 
     await axios.post(slackWebhookUrl, slackMessage);

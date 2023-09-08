de76c07206dd724e20aef16b090e5f69851b51eb
Knut Melvær <knut@sanity.io>
2023-09-07 21:05:40 -0700

    fix: migrate function

--- api/curate-contribution.ts
+++ api/curate-contribution.ts
@@ -1,24 +1,24 @@
 //V3FIXME
-import sanityClient from '@sanity/client';
-import {VercelRequest, VercelResponse} from '@vercel/node';
-import dotenv from 'dotenv';
+import { createClient } from '@sanity/client'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-export const writeClient = sanityClient({
+export const writeClient = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   token: process.env.SANITY_CURATION_WRITE_TOKEN,
   useCdn: false,
   apiVersion: '2022-01-07',
-});
+})
 
-export const readClient = sanityClient({
+export const readClient = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   useCdn: false,
   apiVersion: '2022-01-07',
-});
+})
 
 /**
  * Document types that don't need approval to show up in the website
@@ -28,22 +28,22 @@ const UNCURATED_DOC_TYPES = [
   'contribution.tool',
   'contribution.schema',
   'contribution.guide',
-];
+]
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const {docId, contributionType} = req.query;
+  const { docId, contributionType } = req.query
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
   if (typeof docId !== 'string') {
     return res.status(400).json({
       error: 'Missing doc id',
-    });
+    })
   }
   if (typeof contributionType !== 'string') {
     return res.status(400).json({
       error: 'Missing contribution type',
-    });
+    })
   }
 
   const curatedDoc = {
@@ -57,18 +57,18 @@ export default async (req: VercelRequest, res: VercelResponse) => {
       // Make sure the author can delete their document
       _weak: true,
     },
-  };
+  }
 
   try {
-    await writeClient.createIfNotExists(curatedDoc);
+    await writeClient.createIfNotExists(curatedDoc)
 
     return res.status(200).json({
       success: true,
-    });
+    })
   } catch (error) {
-    console.error(error);
+    console.error(error)
     return res.status(500).json({
       error: "We couldn't create the document",
-    });
+    })
   }
-};
+}

ad0318c065c72ff12a1c379c3c934b96fbc047af
Knut Melvær <knut@sanity.io>
2023-09-07 21:04:33 -0700

    fix: import cli client

--- src/utils/migrateTags.ts
+++ src/utils/migrateTags.ts
@@ -1,46 +1,45 @@
 //V3FIXME
-import {uuid} from '@sanity/uuid';
-import cq from 'concurrent-queue';
-import {useClient} from 'sanity';
-// @ts-expect-error
-//V3FIXME
-const client = useClient().withConfig({apiVersion: '2021-03-25'});
+import { getCliClient } from '@sanity/cli'
+import { uuid } from '@sanity/uuid'
+import cq from 'concurrent-queue'
+
+const client = getCliClient().withConfig({ apiVersion: '2021-03-25' })
 // Create a queue to limit the rate at which you write changes to Sanity
 const queue = cq()
-  .limit({concurrency: 2})
+  .limit({ concurrency: 2 })
   .process(function (task: any) {
     return new Promise(function (resolve, reject) {
-      setTimeout(resolve.bind(undefined, task), 1000);
-    });
-  });
+      setTimeout(resolve.bind(undefined, task), 1000)
+    })
+  })
 
 //We write our query for the document(s) we want to delete
-const query = `{'tickets': *[_type == 'ticket' && defined(tags[0]) && !defined(tags[0]._ref)], 'tags': *[_type == 'tag']}`;
+const query = `{'tickets': *[_type == 'ticket' && defined(tags[0]) && !defined(tags[0]._ref)], 'tags': *[_type == 'tag']}`
 
 const migrateTags = async () => {
   // Use the configured Studio client to fetch our documents
-  const {tickets, tags} = await client.fetch(query).catch((err) => console.log(err));
+  const { tickets, tags } = await client.fetch(query).catch((err) => console.log(err))
 
   for (const ticket of tickets) {
     queue(ticket).then(async () => {
       const updatedTags = ticket.tags.map((ticketTag: any) => {
-        const ref = tags.find((tag: any) => tag.value.current == ticketTag.value);
-        return {_type: 'reference', _ref: ref._id, _key: uuid()};
-      });
+        const ref = tags.find((tag: any) => tag.value.current == ticketTag.value)
+        return { _type: 'reference', _ref: ref._id, _key: uuid() }
+      })
 
       await client
         .patch(ticket._id)
-        .set({tags: updatedTags})
+        .set({ tags: updatedTags })
         .commit()
         .then((updatedDoc) => `Hurray, the bike is updated! New document: ${updatedDoc._id}`)
         .catch((err) => {
-          console.error('Oh no, the update failed: ', err.message);
-        });
-    });
+          console.error('Oh no, the update failed: ', err.message)
+        })
+    })
   }
-};
+}
 
-migrateTags();
+migrateTags()
 
 // execute this script by running
 // $ sanity exec ./lib/utils/batchDelete.js --withUserToken

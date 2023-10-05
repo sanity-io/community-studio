df7af79021bd7f24cdac8957c7f14b2ac0caf05d
Knut Melvær <knut@sanity.io>
2023-10-05 10:04:32 -0700

    fix: include src in build

---
+++ api/_roles/agent.ts
@@ -0,0 +1,27 @@
+/**
+ * TODO: Deprecate this group
+ */
+type Grants = {
+  path?: string
+  filter?: string
+  permissions: string[]
+}
+
+type Agent = {
+  _id: string
+  _type: string
+  grants?: Grants[]
+  members: string[]
+}
+
+export const agent: Agent = {
+  _id: '_.groups.agent',
+  _type: 'system.group',
+  grants: [
+    {
+      path: '**',
+      permissions: ['create', 'read', 'update', 'editHistory', 'history'],
+    },
+  ],
+  members: [],
+}
---
+++ api/_roles/contributor.ts
@@ -0,0 +1,39 @@
+/**
+ * This is the base group settings for the contributors role
+ */
+type Grants = {
+  path?: string
+  filter?: string
+  permissions: string[]
+}
+
+type Contributor = {
+  _id: string
+  _type: string
+  grants?: Grants[]
+  members: string[]
+}
+
+export const contributor: Contributor = {
+  _id: '_.groups.contributor',
+  _type: 'system.group',
+  grants: [
+    {
+      path: '**',
+      permissions: ['history', 'read'],
+    },
+    {
+      filter: '_type match "contribution.*" && identity() in authors[]._ref',
+      permissions: ['create', 'update'],
+    },
+    {
+      filter: '_id == identity() || _id == "drafts." + identity()',
+      permissions: ['create', 'update'],
+    },
+    {
+      filter: '_type in ["sanity.fileAsset", "sanity.imageAsset"]',
+      permissions: ['create', 'update'],
+    },
+  ],
+  members: [],
+}

e9c86c289fab1c31c00f76698b4d7dfb15659aa9
Knut Melvær <knut@sanity.io>
2023-10-05 11:15:54 -0700

    fix: check schematype and not current value

--- schemas/components/PathInput.tsx
+++ schemas/components/PathInput.tsx
@@ -1,53 +1,54 @@
-import { Container, Inline, Text,Card } from '@sanity/ui';
-import React, { useCallback } from 'react';
-import {PatchEvent, set, unset, SlugInputProps, StringInputProps, FormPatch} from 'sanity';
-import speakingurl from 'speakingurl';
-
+import { Container, Inline, Text, Card } from '@sanity/ui'
+import React, { useCallback } from 'react'
+import { PatchEvent, set, unset, SlugInputProps, StringInputProps, FormPatch } from 'sanity'
+import speakingurl from 'speakingurl'
 
 type Slug = {
-  _type: string;
-  current: string;
-};
+  _type: string
+  current: string
+}
 
 //V3FIXME
 // @TODO: generate button (?); proper validation highlighting
-export function PathInput(props: SlugInputProps|StringInputProps) {
-  const {onChange, value, schemaType} = props;
+export function PathInput(props: SlugInputProps | StringInputProps) {
+  const { onChange, value, schemaType } = props
   // @ts-expect-error
-  const {basePath, formatSlugOnBlur} = schemaType.options;
+  const { basePath, formatSlugOnBlur } = schemaType.options
 
-  // @ts-expect-error
-  const isSlug = !!value?.current;
+  const isSlug = schemaType.jsonType === 'object' && schemaType.name === 'slug'
 
+  const handleChange = useCallback(
+    (event: React.FormEvent<HTMLInputElement>) => {
+      const value = event.currentTarget.value
+      let currentValue = value
+      // @ts-expect-error
+      const customFormat = schemaType.options?.customFormat
 
-  const handleChange = useCallback((event: React.FormEvent<HTMLInputElement>) => {
-    const value = event.currentTarget.value
-    let currentValue = value;
-    // @ts-expect-error
-    const customFormat = schemaType.options?.customFormat;
-    console.log('customFormat', customFormat)
-    if (customFormat) {
-      currentValue  = customFormat(currentValue)
-    }
-    if (formatSlugOnBlur) {
-      // Removing special characters, spaces, slashes, etc.
-      currentValue = speakingurl(currentValue, {symbols: true});
-    }
+      if (customFormat) {
+        currentValue = customFormat(currentValue)
+      }
+      if (formatSlugOnBlur) {
+        // Removing special characters, spaces, slashes, etc.
+        currentValue = speakingurl(currentValue, { symbols: true })
+      }
 
-    const patchValue = isSlug ? {_type: schemaType.name, current: currentValue} : currentValue;
+      const patchValue = isSlug ? { _type: schemaType.name, current: currentValue } : currentValue
 
-  onChange(PatchEvent.from(value ? set(patchValue) : unset()));
-}, [onChange, isSlug, schemaType.name]);
-
-
+      onChange(PatchEvent.from(value ? set(patchValue) : unset()))
+    },
+    [onChange, isSlug, schemaType.name],
+  )
 
   return (
     <Inline>
-      <Card padding={3} style={{backgroundColor: '#e6e8ec'}} >
+      <Card padding={3} style={{ backgroundColor: '#e6e8ec' }}>
         <Text size={1}>{basePath}</Text>
       </Card>
-      {/* @ts-expect-error */}
-      {props.renderDefault({...props, elementProps: {...props.elementProps, onChange: handleChange, }})}
+      {props.renderDefault({
+        ...props,
+        /* @ts-expect-error */
+        elementProps: { ...props.elementProps, onChange: handleChange },
+      })}
     </Inline>
-  );
+  )
 }

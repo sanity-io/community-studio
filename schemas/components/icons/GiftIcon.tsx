c057bf0e4551ed8d8fc94c0a976c365abb0b6dfc
Knut Melvær <knut@sanity.io>
2023-10-18 14:57:21 -0700

    chore: fix JSX syntax

--- schemas/components/icons/GiftIcon.tsx
+++ schemas/components/icons/GiftIcon.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from 'react'
 
 export const GiftIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon-tabler icon-tabler-gift"
+    className="icon icon-tabler icon-tabler-gift"
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    stroke-width="2"
+    strokeWidth="2"
     stroke="currentColor"
     fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
     <rect x="3" y="8" width="18" height="4" rx="1"></rect>
@@ -19,4 +19,4 @@ export const GiftIcon = () => (
     <path d="M19 12v7a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-7"></path>
     <path d="M7.5 8a2.5 2.5 0 0 1 0 -5a4.8 8 0 0 1 4.5 5a4.8 8 0 0 1 4.5 -5a2.5 2.5 0 0 1 0 5"></path>
   </svg>
-);
+)

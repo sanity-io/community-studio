a4860c7cda002623969775547d35511eeaa4fab1
Knut Melvær <knut@sanity.io>
2024-06-15 10:31:25 +0200

    chore: fix typings

--- schemas/documents/answerFeedback.tsx
+++ schemas/documents/answerFeedback.tsx
@@ -1,22 +1,31 @@
-import {Card, Text} from '@sanity/ui';
+import { ComponentType } from 'react'
+import { Card, Text } from '@sanity/ui'
+import { DefaultPreviewProps, defineField, PreviewProps } from 'sanity'
 
-export const ratingValue = ['-1', '0', '1'] as const;
+export const ratingValue = ['-1', '0', '1'] as const
 
-export const ratings: any = {
+export const ratings: Record<string, string> = {
   '-1': '🙁',
-  0: '🙂',
-  1: '🤩',
-};
+  '0': '🙂',
+  '1': '🤩',
+}
 
-const FeedbackPreview = ({value}: {value: string}) => {
+// Define a type for the keys of the ratings object
+type RatingKey = keyof typeof ratings
+
+// Define the props type for the FeedbackPreview component
+export type FeedbackPreviewProps = {
+  rating: RatingKey
+}
+
+// @ts-expect-error
+const FeedbackPreview = ({ rating }: ComponentType<PreviewProps<FeedbackPreviewProps>>) => {
   return (
     <Card>
-      <Text size={4}>{ratings[value]}</Text>
+      <Text size={4}>{ratings[rating]}</Text>
     </Card>
-  );
-};
-
-FeedbackPreview.displayName = 'FeedbackPreview';
+  )
+}
 
 export const answerFeedback = {
   title: 'Answer feedback',
@@ -30,45 +39,53 @@ export const answerFeedback = {
       _createdAt: '_createdAt',
     },
     prepare(selection: any) {
-      const {title = '[No comment]', rating, answerTitle, _createdAt} = selection;
+      const { title = '[No comment]', rating, answerTitle, _createdAt } = selection
 
       return {
         title,
         subtitle: `${answerTitle} - ${new Date(_createdAt).toLocaleString()}`,
         media: <h1>{ratings[rating]}</h1>,
-      };
+      }
     },
   },
   fields: [
-    {
+    defineField({
       title: 'Rating',
       name: 'rating',
       type: 'number',
-      inputComponent: FeedbackPreview,
-    },
-    {
+      components: {
+        // @ts-expect-error
+        preview: FeedbackPreview,
+      },
+      preview: {
+        select: {
+          rating: 'rating',
+        },
+      },
+    }),
+    defineField({
       title: 'Answer',
       name: 'answer',
       type: 'reference',
       weak: true,
-      to: {type: 'editorial'},
-    },
-    {
+      to: { type: 'editorial' },
+    }),
+    defineField({
       title: 'Comment',
       name: 'comment',
       type: 'text',
       description: 'Comment submitted from sanity.io',
-    },
-    {
+    }),
+    defineField({
       name: 'done',
       type: 'boolean',
       title: 'Is this feedback dealt with?',
-    },
-    {
+    }),
+    defineField({
       name: 'notes',
       type: 'text',
       title: 'Internal notes',
       description: 'What did we do with this feedback?',
-    },
+    }),
   ],
-};
+}

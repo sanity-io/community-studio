142141aebf29aaee6aa338c49b719a6ae013e295
Geoff Ball <geoff@sanity.io>
2024-12-30 11:39:02 -0700

    Allow Sanity administrators to delete users in Studio (#165)
    Updates the document actions permissions to allow Sanity administrators to delete users from within the Studio (currently, this must be done using the CLI).

--- plugins/actions/index.ts
+++ plugins/actions/index.ts
@@ -2,7 +2,9 @@ import {DocumentActionsResolver} from 'sanity';
 import PublishContributionAction from './publishContributionAction';
 import PublishTicketAction from './publishTicketAction';
 
-export const resolveDocumentActions: DocumentActionsResolver = (prev, {schemaType}) => {
+export const resolveDocumentActions: DocumentActionsResolver = (prev, context) => {
+  const {currentUser, schemaType} = context;
+
   // Contribution documents need a distinct publish action for curatedContribution creation
   if (schemaType.includes('contribution.')) {
     return [PublishContributionAction, ...prev.filter(({action}) => action !== 'publish')];
@@ -11,6 +13,15 @@ export const resolveDocumentActions: DocumentActionsResolver = (prev, {schemaTyp
   // Tickets have an auto-generated slug, hence the custom publish action
   if (schemaType.includes('ticket')) {
     return [PublishTicketAction, ...prev.filter(({action}) => action !== 'publish')];
+  }
+
+  // Permit Sanity administrators to delete users
+  if (
+    schemaType === 'person' &&
+    currentUser?.roles.some((role) => role?.name === 'administrator') &&
+    currentUser?.email.endsWith('@sanity.io')
+  ) {
+    return prev;
   }
 
   // Non-deletable documents
